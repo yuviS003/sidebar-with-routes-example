700c359c459617c3af4b27ecef4277a6076dd153
Yuvraj Singh <yuvi7860808034@gmail.com>
2023-01-03 16:18:00 +0530

    initial-commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,83 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import Box from '@mui/material/Box';
+import Drawer from '@mui/material/Drawer';
+import AppBar from '@mui/material/AppBar';
+import CssBaseline from '@mui/material/CssBaseline';
+import Toolbar from '@mui/material/Toolbar';
+import List from '@mui/material/List';
+import Typography from '@mui/material/Typography';
+import Divider from '@mui/material/Divider';
+import './App.css'
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Link
+} from "react-router-dom";
+
+import Home from './components/Home'
+import About from './components/About'
+import Contact from './components/Contact'
+import Services from './components/Services'
+
+const drawerWidth = 240;
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <Router>
+      <Box sx={{ display: 'flex' }}>
+        <CssBaseline />
+        <AppBar position="fixed" sx={{ zIndex: (theme) => theme.zIndex.drawer + 1 }}>
+          <Toolbar>
+            <Typography variant="h6" noWrap component="div">
+              Clipped drawer
+            </Typography>
+          </Toolbar>
+        </AppBar>
+        <Drawer
+          variant="permanent"
+          sx={{
+            width: drawerWidth,
+            flexShrink: 0,
+            [`& .MuiDrawer-paper`]: { width: drawerWidth, boxSizing: 'border-box' },
+          }}
         >
-          Learn React
-        </a>
-      </header>
-    </div>
+          <Toolbar />
+          <Box sx={{ overflow: 'auto' }}>
+            <List>
+              <Link to='/'>
+                <div className="nav-links">Home</div>
+              </Link>
+              <Divider />
+              <Link to='/about'>
+                <div className="nav-links">About</div>
+              </Link>
+              <Divider />
+              <Link to='/contact'>
+                <div className="nav-links">Contact</div>
+              </Link>
+              <Divider />
+              <Link to='/services'>
+                <div className="nav-links">Services</div>
+              </Link>
+              <Divider />
+            </List>
+          </Box>
+        </Drawer>
+        <Box component="main" sx={{ flexGrow: 1, p: 3 }}>
+          <Toolbar />
+          <Routes>
+            <Route exact path="/" element={<Home />} />
+            <Route exact path="/about" element={<About />} />
+            <Route exact path="/contact" element={<Contact />} />
+            <Route exact path="/services" element={<Services />} />
+          </Routes>
+
+        </Box>
+      </Box>
+    </Router>
+
   );
 }
 
